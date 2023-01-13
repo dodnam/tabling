bc6f75c7bd32699f86194aa8384f50073bf91a9f
ghryu <dodnam@naver.com>
2023-01-13 20:30:54 +0900

    errorCode #1

--- src/main/java/com/nam/tabling/controller/BaseController.java
+++ src/main/java/com/nam/tabling/controller/BaseController.java
@@ -2,9 +2,11 @@ package com.nam.tabling.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@ControllerAdvice(basePackageClasses = BaseController.class) // BaseController가 속해있는 패키지가 범위임을 의미함
 @Controller // Controller bean으로 스프링에 등록된다.
 public class BaseController implements ErrorController {
 
