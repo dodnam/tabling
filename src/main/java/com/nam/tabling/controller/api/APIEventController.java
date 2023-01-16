0d474cdd2c679726e7b73a7d9c38d50b741db0ee
ghryu <dodnam@naver.com>
2023-01-16 11:32:40 +0900

    errorCode #3

--- src/main/java/com/nam/tabling/controller/api/APIEventController.java
+++ src/main/java/com/nam/tabling/controller/api/APIEventController.java
@@ -5,6 +5,7 @@ import com.nam.tabling.dto.APIErrorResponse;
 import com.nam.tabling.exception.GeneralException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,18 +15,21 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() {
-        return List.of("event1", "event2");
+    public List<String> getEvents() throws Exception{
+        throw new HttpRequestMethodNotSupportedException("405 에러 테스트");
+//        return List.of("event1", "event2");
     }
 
     @PostMapping("/events")
     public Boolean createEvent() {
-        return true;
+        throw new GeneralException("제너럴 에러");
+//        return true;
     }
 
     @GetMapping("/events/{eventId}")
     public String getEvent(@PathVariable Integer eventId) {
-        return "event" + eventId;
+        throw new RuntimeException("런타임 에러");
+//        return "event" + eventId;
     }
 
     @PutMapping("/events/{eventId}")
--- src/main/java/com/nam/tabling/controller/error/APIExceptionHandler.java
+++ src/main/java/com/nam/tabling/controller/error/APIExceptionHandler.java
@@ -3,35 +3,58 @@ package com.nam.tabling.controller.error;
 import com.nam.tabling.constant.ErrorCode;
 import com.nam.tabling.dto.APIErrorResponse;
 import com.nam.tabling.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice(annotations = RestController.class) // Rest컨트롤러의 동작을 감시 + ResponseBody
-public class APIExceptionHandler {
-
+public class APIExceptionHandler extends ResponseEntityExceptionHandler {
+// ResponseEntityExceptionHandler : Spring mvc를 사용할 때 나타나는 exception을 핸들링 한다.
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException e) {
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
 
         ErrorCode errorCode = e.getErrorcode();
         HttpStatus status = errorCode.isClientSideError() ?
                 HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(false, errorCode));
-
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
     }
 
     @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> exception(Exception e) {
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(false, errorCode));
+        return super.handleExceptionInternal(
+                e,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(e)),
+                HttpHeaders.EMPTY,
+                status,
+                request
+        );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorCode errorCode = status.is4xxClientError() ?
+                ErrorCode.SPRING_BAD_REQUEST : ErrorCode.SPRING_INTERNAL_ERROR;
+
+        return super.handleExceptionInternal(
+                ex,
+                APIErrorResponse.of(false, errorCode.getCode(), errorCode.getMessage(ex)),
+                headers,
+                status,
+                request);
     }
 }
